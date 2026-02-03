ecab8843992c0d8a025f53626bb3f2594aa74dc4
liudong <liudong@xaidc.com>
2026-02-03 13:45:09 +0800

    新增tag构建

--- main.go
+++ main.go
@@ -2,11 +2,24 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+var (
+	Version   = "dev"
+	GitCommit = "unknown"
+	BuildTime = "unknown"
+)
+
 func main() {
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
+		})
+	})
+	router.GET("/version", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"version":   Version,
+			"gitCommit": GitCommit,
+			"buildTime": BuildTime,
 		})
 	})
 	router.Run()
